fc429ffa0e3475f308d48df8e18d19f4bbc845ee
Stewart Duffey <duffey.sj@gmail.com>
2015-02-10 09:58:37 +0000

    Add list lookup actor

--- src/main/scala/com/example/ListCon.scala
+++ src/main/scala/com/example/ListCon.scala
@@ -1,10 +1,12 @@
 package com.example
 
-import akka.actor.{ Actor, ActorLogging }
+import akka.actor.{ Actor, ActorRef, ActorLogging, Props }
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
 
 object UrlList {
 	case class Url(url: String)
+	case class CreateList(uuid:String, title:String)
 	case class AddUrlToList(uuid: String, url: String)
 }
 
@@ -22,6 +24,22 @@ trait UrlList { this: Actor =>
 class UrlListManager extends Actor with ActorLogging with UrlList {
 
 	def receive = urlListReceive
+}
+
+trait UrlListLookup { this: Actor =>
+	import UrlList._
+
+	val lists = Map[String, ActorRef]()
+	
+	def urlListLookupReceive: Receive = {
+		case CreateList(uuid, title) =>
+			val newList = context.actorOf(Props[UrlListManager], s"url-list-${uuid}")
+			lists += (uuid -> newList)
+	}
+}
+
+class UrlListLookupManager extends Actor with UrlListLookup {
+	def receive = urlListLookupReceive
 }
 
 /**
--- src/test/scala/com/example/UrlListSpec.scala
+++ src/test/scala/com/example/UrlListSpec.scala
@@ -9,6 +9,10 @@ class TestUrlList extends Actor with UrlList {
     def receive = urlListReceive
 }
 
+class TestUrlListLookup extends Actor with UrlListLookup {
+    def receive = urlListLookupReceive
+}
+
 class UrlListSpec extends TestKit(ActorSystem("UrlListSpec"))
     with WordSpecLike
     with MustMatchers
@@ -23,8 +27,20 @@ class UrlListSpec extends TestKit(ActorSystem("UrlListSpec"))
             val real = TestActorRef[TestUrlList].underlyingActor
             real.receive(AddUrlToList("xxx", "http://www.google.com"))
             real.urls must contain (Url("http://www.google.com"))
-            // first actor creates a urllist actor
-            // this one just handles messages to the urllist
         }
+    }
+
+    "UrlListCreator" should {
+    	"create a new list" in {
+    		val lookupRef = TestActorRef[TestUrlListLookup]
+    		val lookup = lookupRef.underlyingActor
+
+    		lookup.lists must have size (0)
+
+    		lookup.receive(CreateList("xxx", "My New List"))
+
+    		lookup.lists must contain key "xxx"
+    		lookup.lists must have size (1)
+    	}
     }
 }
